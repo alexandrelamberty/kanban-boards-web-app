7089a954dc3bac217bf01c7c7883c6980f5f71c7
Alexandre Lamberty <alexandrelamberty@gmail.com>
2023-01-24 17:04:51 +0100

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    projectId: 'angular-board-issues-53ae5',
-    appId: '1:1041589568026:web:ffe6ce1c298408b3e907a3',
-    storageBucket: 'angular-board-issues-53ae5.appspot.com',
-    apiKey: 'AIzaSyAfDe5_k5r9soy1mJvZwBeGdDAkqSrZg7Y',
-    authDomain: 'angular-board-issues-53ae5.firebaseapp.com',
-    messagingSenderId: '1041589568026',
+    projectId: '',
+    appId: '',
+    storageBucket: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
   },
 };
